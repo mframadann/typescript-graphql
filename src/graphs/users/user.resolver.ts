ec32ff037cc7cf3706698c397cf3125eda1bf619
Daanjs <dan.alphadev@gmail.com>
2023-01-31 12:02:31 +0700

    add commands

--- src/graphs/users/user.resolver.ts
+++ src/graphs/users/user.resolver.ts
@@ -12,20 +12,34 @@ export default {
     });
     return users;
   },
+  findUserById: async (arg: UsersInputs): Promise<any> => {
+    const user_id = Number(arg.userInputs.user_id);
+    console.log(arg);
+
+    const user = await prisma.users.findUnique({
+      where: {
+        user_id,
+      },
+      include: {
+        profile: true,
+      },
+    });
+    return user;
+  },
   createUsers: async (args: UsersInputs): Promise<any> => {
     const { firstName, lastName, emailAddress, password, dateOfBirth, gender } =
       args.userInputs;
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(String(password), 12);
     const newUsers = await prisma.users.create({
       data: {
-        email_address: emailAddress,
+        email_address: String(emailAddress),
         password: String(hashedPassword),
         profile: {
           create: {
-            first_name: firstName,
-            last_name: lastName,
-            date_of_birth: new Date(dateOfBirth),
-            gender: gender,
+            first_name: String(firstName),
+            last_name: String(lastName),
+            date_of_birth: new Date(String(dateOfBirth)),
+            gender: String(gender),
           },
         },
       },
